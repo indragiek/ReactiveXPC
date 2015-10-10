32badd93c3f1c312bd7a029a8138571a7f85416a
Indragie Karunaratne <i@indragie.com>
2015-10-09 21:41:52 -0600

    Initial Commit

---
+++ ReactiveXPC/ReactiveXPC.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveXPC.h
+//  ReactiveXPC
+//
+//  Created by Indragie on 10/9/15.
+//  Copyright © 2015 Indragie Karunaratne. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ReactiveXPC.
+FOUNDATION_EXPORT double ReactiveXPCVersionNumber;
+
+//! Project version string for ReactiveXPC.
+FOUNDATION_EXPORT const unsigned char ReactiveXPCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveXPC/PublicHeader.h>
+
+
